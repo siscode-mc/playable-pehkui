def463a839da0bd558a695ef23b92f24a79dc346
alikindsys <alice@blocovermelho.org>
2026-04-07 22:05:44 -0300

    Setup Forge Environment

---
+++ src/main/java/org/siscode/playablePehkui/platform/forgelike/PlayablePehkui.java
@@ -0,0 +1,19 @@
+package org.siscode.playablePehkui.platform.forgelike;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("playable_pehkui")
+public class PlayablePehkui {
+    public PlayablePehkui(FMLJavaModLoadingContext context) {
+        // Register the mod onto Minecraft Forge
+        MinecraftForge.EVENT_BUS.register(this);
+        context.getModEventBus().addListener(this::onCommonSetup);
+    }
+
+    private void onCommonSetup(final FMLCommonSetupEvent event) {
+        // Do whatever.
+    }
+}
