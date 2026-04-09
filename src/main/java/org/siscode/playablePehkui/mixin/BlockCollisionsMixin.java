bcb3e64a92d65b057256f13b2d31f9e13367be72
alikindsys <alice@blocovermelho.org>
2026-04-09 01:22:41 -0300

    Disable block collisions for beegs on leaves
    Injectors:
    - pphk$leavesHaveNoColissionIfYoureBig

---
+++ src/main/java/org/siscode/playablePehkui/mixin/BlockCollisionsMixin.java
@@ -0,0 +1,35 @@
+package org.siscode.playablePehkui.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockCollisions;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.siscode.playablePehkui.util.PehkuiUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import virtuoel.pehkui.api.ScaleTypes;
+
+@Mixin(BlockCollisions.class)
+public class BlockCollisionsMixin<T>  {
+    @WrapOperation(method = "computeNext", at = @At(value = "INVOKE", target="Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
+    public VoxelShape pphk$leavesHaveNoColissionIfYoureBig(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext, Operation<VoxelShape> original) {
+        if (!(collisionContext instanceof EntityCollisionContext)) { return original.call(blockState, blockGetter, blockPos, collisionContext); }
+        Entity entity = ((EntityCollisionContext) collisionContext).getEntity();
+        if (entity == null) { return original.call(blockState, blockGetter, blockPos, collisionContext); }
+        double scale = PehkuiUtil.GetEntityScale(entity, ScaleTypes.BASE);
+        if (scale >= 2) {
+            if (blockState.is(BlockTags.LEAVES)) {
+                return Shapes.empty();
+            }
+        }
+        return original.call(blockState, blockGetter, blockPos, collisionContext);
+    }
+}
