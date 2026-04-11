38396696f57d4ac3df71fbe36dce470873863e16
wffirilat <wffirilat@users.noreply.github.com>
2026-04-11 17:40:55 -0500

    Learn to render boxes into the world. Not yet actually used.

---
+++ src/main/java/org/siscode/playablePehkui/render/BoxRenderer.java
@@ -0,0 +1,70 @@
+package org.siscode.playablePehkui.render;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.ArrayList;
+
+public class BoxRenderer {
+    public static ArrayList<AABB> boxesToRender = new ArrayList<>();
+
+    public static void renderBoxes(PoseStack poseStack, Camera camera) {
+        for (var box : boxesToRender) {
+            renderBox(box, poseStack, camera);
+        }
+//        var block = debug_getBlockBox();
+//        if (block != null) {
+//            renderBox(block, poseStack, camera);
+//        }
+        boxesToRender.clear();
+    }
+
+    private static void renderBox(AABB box, PoseStack poseStack, Camera camera) {
+        Minecraft mc = Minecraft.getInstance();
+
+        Vec3 cameraPos = camera.getPosition();
+        AABB cameraspaceBox = box.move(-cameraPos.x, -cameraPos.y, -cameraPos.z);
+
+        poseStack.pushPose();
+
+        RenderSystem.depthMask(false);
+        RenderSystem.disableDepthTest();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.lineWidth(2.0f);
+
+        VertexConsumer lines = mc.renderBuffers().bufferSource().getBuffer(RenderType.LINES);
+
+        LevelRenderer.renderLineBox(
+                poseStack,
+                lines,
+                cameraspaceBox,
+                1f, 1f, 1f, 1f
+        );
+
+        mc.renderBuffers().bufferSource().endBatch(RenderType.LINES);
+
+        RenderSystem.enableDepthTest();
+        RenderSystem.depthMask(true);
+
+        poseStack.popPose();
+    }
+
+    private static AABB debug_getBlockBox() {
+        Minecraft mc = Minecraft.getInstance();
+
+        var hit = mc.hitResult;
+        if (!(hit instanceof BlockHitResult)) { return null; }
+        var block = (BlockHitResult) hit;
+
+        return AABB.unitCubeFromLowerCorner(Vec3.atLowerCornerOf(block.getBlockPos()));
+    }
+}
