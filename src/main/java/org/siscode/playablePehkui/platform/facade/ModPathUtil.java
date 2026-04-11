db217fa88d9c4a42e12fe83a66e5862c5d62a9fe
alikindsys <alice@blocovermelho.org>
2026-04-11 02:23:05 -0300

    Implement forgelike ModPathUtil

--- src/main/java/org/siscode/playablePehkui/platform/facade/ModPathUtil.java
+++ src/main/java/org/siscode/playablePehkui/platform/facade/ModPathUtil.java
@@ -4,6 +4,6 @@ import java.nio.file.Path;
 
 public class ModPathUtil {
     public static Path pphkGetModConfigFolder() {
-        return org.siscode.playablePehkui.platform.fabric.ModPathUtil.pphkGetModConfigFolder_fabric();
+        return org.siscode.playablePehkui.platform.forgelike.ModPathUtil.pphkGetModConfigFolder_forgelike();
     }
 }
