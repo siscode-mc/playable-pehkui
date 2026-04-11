cec7af56fa251475b0225aee7f993a76e6aeabd3
alikindsys <alice@blocovermelho.org>
2026-04-11 02:21:11 -0300

    Remove fabric-specific unused import

--- src/main/java/org/siscode/playablePehkui/mixin/LivingEntityMixin.java
+++ src/main/java/org/siscode/playablePehkui/mixin/LivingEntityMixin.java
@@ -13,7 +13,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import org.siscode.playablePehkui.config.ServerConfig;
 import org.siscode.playablePehkui.movement.ScaleSensitiveClimbables;
-import org.siscode.playablePehkui.platform.fabric.PlayablePehkui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
