73a82ac388793c8075fbb5b380c25bf8186ca9c1
alikindsys <alice@blocovermelho.org>
2026-04-13 18:35:34 -0300

    Add category map accessor

--- src/main/java/org/siscode/playablePehkui/mixin/KeyMappingAccessor.java
+++ src/main/java/org/siscode/playablePehkui/mixin/KeyMappingAccessor.java
@@ -5,8 +5,15 @@ import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import java.util.Map;
+
 @Mixin(KeyMapping.class)
 public interface KeyMappingAccessor {
     @Accessor("key")
     InputConstants.Key pphk$currentKey();
+
+    @Accessor("CATEGORY_SORT_ORDER")
+    static Map<String, Integer> pphk$getCategoryMap() {
+        throw new UnsupportedOperationException();
+    }
 }
