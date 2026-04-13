e813cf7c9648e6e2582568bdf2f60e6d17b604fa
alikindsys <alice@blocovermelho.org>
2026-04-13 00:23:50 -0300

    Add KeyMapping type

---
+++ src/main/java/org/siscode/playablePehkui/keymapping/KeyMapping.java
@@ -0,0 +1,63 @@
+package org.siscode.playablePehkui.keymapping;
+
+
+/**
+ * Represents a custom keymapping that's cross-platform and have a better API.
+ */
+public class KeyMapping {
+    public static final String DEFAULT_CATEGORY = "key.playablepehkui";
+
+    public int ticksSinceLastPress = Integer.MAX_VALUE;
+    public boolean isPressed = false;
+    public int keycode;
+    public boolean showInConfigScreen;
+    public String translationKey;
+    public String category;
+
+    public KeyMapping(String translationKey, int keycode, boolean showInConfigScreen) {
+        this.translationKey = translationKey;
+        this.category = DEFAULT_CATEGORY;
+        this.keycode = keycode;
+        this.showInConfigScreen = showInConfigScreen;
+
+        KeyHandler.put(this);
+    }
+
+    public KeyMapping(String translationKey, String category, int keycode, boolean showInConfigScreen) {
+        this.translationKey = translationKey;
+        this.category = category;
+        this.keycode = keycode;
+        this.showInConfigScreen = showInConfigScreen;
+
+        KeyHandler.put(this);
+    }
+
+    public void setPressed(boolean value) {
+        this.isPressed = value;
+        if (value) {
+            this.ticksSinceLastPress = 0;
+        }
+    }
+
+    public void tick() {
+        if (ticksSinceLastPress != Integer.MAX_VALUE) {
+            this.ticksSinceLastPress += 1;
+        }
+    }
+
+    public void onKeyPress(KeyEvents.Keypress event) {
+        KeyHandler.register(this, event);
+    }
+
+    public void onScreenKeypress(KeyEvents.ScreenKeypress event) {
+        KeyHandler.register(this, event);
+    }
+
+    public void onLevelKeypress(KeyEvents.LevelKeypress event) {
+        KeyHandler.register(this, event);
+    }
+
+    public void onTitleScreenKeypress(KeyEvents.TitleScreenKeypress event) {
+        KeyHandler.register(this, event);
+    }
+}
